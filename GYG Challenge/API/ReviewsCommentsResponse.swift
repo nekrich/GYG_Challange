cf6a9eb1cc2bb2d94e650a0e3042f0938ae4acf5
Vitalii Budnik <inekrich@me.com>
2019-06-01 18:39:54 +0300

    Update ReviewsCommentsResponse

--- GYG Challenge/API/ReviewsCommentsResponse.swift
+++ GYG Challenge/API/ReviewsCommentsResponse.swift
@@ -5,6 +5,7 @@ struct ReviewsCommentsResponse {
   let status: Bool
   let totalReviewsCount: Int
   let reviews: [ReviewComment]
+  let message: String?
 }
 
 extension ReviewsCommentsResponse: Decodable {
@@ -14,18 +15,22 @@ extension ReviewsCommentsResponse: Decodable {
     case status = "status"
     case totalReviewsCount = "total_reviews_comments"
     case reviews = "data"
+    case message = "message"
   }
   
   init(from decoder: Decoder) throws {
     let container = try decoder.container(keyedBy: CodingKey.self)
     
     status = try container.decode(Bool.self, forKey: .status)
+    
     if status {
       totalReviewsCount = try container.decode(Int.self, forKey: .totalReviewsCount)
       reviews = try container.decode([ReviewComment].self, forKey: .reviews)
+      message = .none
     } else {
       totalReviewsCount = 0
       reviews = []
+      message = try container.decodeIfPresent(String.self, forKey: .message) ?? "Something went wrong"
     }
   }
 }
