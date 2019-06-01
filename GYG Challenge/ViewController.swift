8c702654a19beb1596895f26f20895a10cc0709c
Vitalii Budnik <inekrich@me.com>
2019-06-01 11:11:48 +0300

    Initial Commit

---
+++ GYG Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GYG Challange
+//
+//  Created by Vitalii Budnik on 6/1/19.
+//  Copyright © 2019 Vitalii Budnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
