f1bdcbead7fc52de738806e20da1d5e927c69135
Vitalii Budnik <inekrich@me.com>
2019-06-02 14:51:08 +0300

    Fix raiting and message in ReviewComment

--- GYG Challenge/Models/ReviewComment.swift
+++ GYG Challenge/Models/ReviewComment.swift
@@ -3,13 +3,13 @@ import Foundation
 struct ReviewComment {
   
   let id: UInt64
-  let rating: String
+  let rating: Float
   let date: Date
   
   let reviwer: Reviewer
   
   let title: String?
-  let message: String
+  let message: String?
   let languageCode: String
 }
 
@@ -30,13 +30,25 @@ extension ReviewComment: Decodable {
     let container = try decoder.container(keyedBy: CodingKey.self)
     
     id = try container.decode(UInt64.self, forKey: .id)
-    rating = try container.decode(String.self, forKey: .rating)
+    
+    let rating = Float(try container.decode(String.self, forKey: .rating)) ?? 0.0
+    
+    if rating < 1.0 || rating > 5.0 {
+      throw DecodingError.dataCorruptedError(
+        forKey: CodingKey.rating,
+        in: container,
+        debugDescription: "Rating \(rating) is out of bounds 1.0...5.0"
+      )
+    }
+    
+    self.rating = rating
+    
     date = try container.decode(Date.self, forKey: .date)
     
     reviwer = try Reviewer(from: decoder)
     
     title = try container.decodeIfPresent(String.self, forKey: .title)
-    message = try container.decode(String.self, forKey: .message)
+    message = try container.decodeIfPresent(String.self, forKey: .message)
     languageCode = try container.decode(String.self, forKey: .languageCode)
   }
 }
